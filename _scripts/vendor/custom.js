6bbf782b7e2a212321121ba6f9d81efec433c3e1
jheftmann <jheftmann@mac.com>
2015-03-11 13:20:53 -0400

    clean up js

--- _scripts/vendor/custom.js
+++ _scripts/vendor/custom.js
@@ -9,49 +9,39 @@ $(document).ready(function() {
 	});
 
 	// set the min height of each section
-	//if (($(window).innerWidth()) > 480) {
+	$(window).resize(function() {
+		var sectionMinHeight = $(window).height();
 
-		$(window).resize(function() {
-			var sectionMinHeight = $(window).height();
+		var sectionWidth = ($('.section-right').width()) * .7;
+		var sectionMaxHeight = sectionMinHeight * .5;
+		var imgContainer = $('.img-container');
 
-			var sectionWidth = ($('.section-right').width()) * .7;
-			var sectionMaxHeight = sectionMinHeight * .5;
-			var imgContainer = $('.img-container');
+		// set height of sections
+		$('.section-inner').css('min-height', sectionMinHeight);
 
-			// set height of sections
-			$('.section-inner').css('min-height', sectionMinHeight);
+		// set size and position of featured image
+		$(imgContainer).css('height', sectionMinHeight);
+		$(imgContainer).css('width', sectionWidth);
+		$(imgContainer).css('max-height', sectionMaxHeight);
 
-			// set size and position of featured image
-			$(imgContainer).css('height', sectionMinHeight);
-			$(imgContainer).css('width', sectionWidth);
-			$(imgContainer).css('max-height', sectionMaxHeight);
+	});
 
-
-		});
-
-/*
-	} else {
-
+	// vertical center
+	function verticalCenter() {
+		var parent = $('.section-right');
+		var child = parent.find('.img-container');
+		var topMargin = (parent.height() - child.height()) / 2;
+	  $(parent).each(function(){
+	    child.css('margin-top', topMargin);
+	  });
 	}
-*/
 
-
-
+	verticalCenter();
+		$(window).resize(function() {
+			verticalCenter();
+	});
 
 	$(window).trigger('resize');
-
-/*
-	$('.section-left').each(function(){
-		var padding = 160;
-		var sectionHeight = $(this).height() + padding;
-		$(this).next('.section-right').find('.img-container').css('height',sectionHeight);
-		$(this).next('.section-right .img-container').css('height',sectionHeight);
-		console.log(sectionHeight);
-	});
-*/
-
-
-	//console.log(sectionHeight);
 
 	// scrolling nav
 	var section_margins = 80;
@@ -74,25 +64,5 @@ $(document).ready(function() {
 	        //I get fired when you enter a section and I pass the list item of the section
 	    }
 	});
-
 });
 
-	function verticalCenter() {
-		var parent = $('.section-right');
-		var child = parent.find('.img-container');
-		var topMargin = (parent.height() - child.height()) / 2;
-	  $(parent).each(function(){
-	    child.css('margin-top', topMargin);
-	  });
-	}
-
-	$(document).ready(function () {
-
-		verticalCenter();
-
-		$(window).resize(function() {
-
-			verticalCenter();
-
-	});
-	});
