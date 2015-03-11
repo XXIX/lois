5d553863cb26948adfd22ce51e1d9ec25c0ffc18
jheftmann <jheftmann@mac.com>
2015-03-10 21:24:50 -0400

    responsive text sizes

--- _scripts/vendor/custom.js
+++ _scripts/vendor/custom.js
@@ -8,6 +8,51 @@ $(document).ready(function() {
 		$('.nav-extended').slideUp('fast');
 	});
 
+	// set the min height of each section
+	//if (($(window).innerWidth()) > 480) {
+
+		$(window).resize(function() {
+			var sectionMinHeight = $(window).height();
+
+			var sectionWidth = ($('.section-right').width()) * .7;
+			var sectionMaxHeight = sectionMinHeight * .5;
+			var imgContainer = $('.img-container');
+
+			// set height of sections
+			$('.section-inner').css('min-height', sectionMinHeight);
+
+			// set size and position of featured image
+			$(imgContainer).css('height', sectionMinHeight);
+			$(imgContainer).css('width', sectionWidth);
+			$(imgContainer).css('max-height', sectionMaxHeight);
+
+
+		});
+
+/*
+	} else {
+
+	}
+*/
+
+
+
+
+	$(window).trigger('resize');
+
+/*
+	$('.section-left').each(function(){
+		var padding = 160;
+		var sectionHeight = $(this).height() + padding;
+		$(this).next('.section-right').find('.img-container').css('height',sectionHeight);
+		$(this).next('.section-right .img-container').css('height',sectionHeight);
+		console.log(sectionHeight);
+	});
+*/
+
+
+	//console.log(sectionHeight);
+
 	// scrolling nav
 	var section_margins = 80;
 	var top_offset = $('nav').height() + section_margins;  // get height of fixed navbar
@@ -31,3 +76,23 @@ $(document).ready(function() {
 	});
 
 });
+
+	function verticalCenter() {
+		var parent = $('.section-right');
+		var child = parent.find('.img-container');
+		var topMargin = (parent.height() - child.height()) / 2;
+	  $(parent).each(function(){
+	    child.css('margin-top', topMargin);
+	  });
+	}
+
+	$(document).ready(function () {
+
+		verticalCenter();
+
+		$(window).resize(function() {
+
+			verticalCenter();
+
+	});
+	});
